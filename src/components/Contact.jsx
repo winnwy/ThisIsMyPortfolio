92b4fed54a5d8537e199b8f4d7c4917e1aa3690c
Winn Z <winnwyz@gmail.com>
2025-02-03 22:44:59 +1100

    Use constant instead of hardcoded link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ import { CONTACT } from "../constants";
 const Contact = () => {
   return (
     <section className="border-b border-neutral-900 pb-20">
-      <div className="container mx-auto px-8">
-        <h1 className="my-10 text-center text-4xl font-bold text-neutral-300">
+      <div className="container mx-auto px-4 sm:px-8">
+        <h1 className="my-10 text-center text-3xl sm:text-4xl font-bold text-neutral-300">
           Get in Touch
         </h1>
         <div className="text-center">
@@ -15,17 +15,17 @@ const Contact = () => {
                 href={`mailto:${CONTACT.email}`}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-neutral-400 hover:text-purple-400 transition-colors duration-300"
+                className="text-neutral-400 hover:text-purple-400 transition-colors duration-300 text-lg sm:text-xl"
               >
                 ✉️ Email: {CONTACT.email}
               </a>
             </li>
             <li>
               <a
-                href={CONTACT.github}
+                href="https://github.com/winnwy"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-neutral-400 hover:text-purple-400 transition-colors duration-300"
+                className="text-neutral-400 hover:text-purple-400 transition-colors duration-300 text-lg sm:text-xl"
               >
                 🐙 GitHub: winnwy
               </a>
