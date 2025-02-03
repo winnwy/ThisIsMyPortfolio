51ada2fbc0056d1631f89318a91c6cfcb958e615
Winn Z <winnwyz@gmail.com>
2025-02-03 22:45:54 +1100

    Use scroll instead of navigating between screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      {/* Background div */}
+      {/* Background */}
       <div className="fixed top-0 left-0 -z-10 h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-      {/* Content container */}
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Sections with IDs */}
       <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
+        <section id="about">
+          <About />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
       </div>
     </div>
   );
